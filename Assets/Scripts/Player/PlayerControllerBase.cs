77519762aef6bc062feb0a260db94140c95503b6
Xiangfei Kong <61561634+DeusPio@users.noreply.github.com>
2023-05-07 22:44:54 -0400

    create playercontrollerbase

---
+++ Assets/Scripts/Player/PlayerControllerBase.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Common;
+using UnityEngine;
+
+public class PlayerControllerBase : MonoSingleton<PlayerControllerBase>
+{
+    public virtual void GameStart()
+    {
+        
+    }
+
+    public virtual void GameEnd()
+    {
+        
+    }
+    
+    
+}
